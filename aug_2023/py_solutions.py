aa63e3ade95211681071f39c0385e7c7c5bb14d7
Simon Conrad <simondbconrad@gmail.com>
2023-08-23 10:11:29 -0400

    add iterative solution

--- aug_2023/py_solutions.py
+++ aug_2023/py_solutions.py
@@ -28,3 +28,23 @@ class Solution(object):
         else:
             list2.next = self.mergeTwoLists(list2.next, list1)
             return list2
+
+# iterative solution:
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy = cur = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        cur.next = l1 or l2
+        return dummy.next
