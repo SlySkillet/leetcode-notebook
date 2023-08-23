b80ca6b75059bfe66849d20b11739742f1e70d7b
Simon Conrad <simondbconrad@gmail.com>
2023-08-23 10:44:22 -0400

    solve 28. find inx of first occurrence in a string

--- aug_2023/py_solutions.py
+++ aug_2023/py_solutions.py
@@ -48,3 +48,18 @@ class Solution(object):
             cur = cur.next
         cur.next = l1 or l2
         return dummy.next
+
+# 28. Find the Index of the First Occurrence in a String
+# https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        needle_size = len(needle)
+        for i in range(len(haystack)):
+            if haystack[i:i+needle_size] == needle:
+                return i
+        return -1
