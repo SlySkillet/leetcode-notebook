e88f89851f6bf47f55acb7f1e7084871a237a084
Simon Conrad <simondbconrad@gmail.com>
2023-08-23 09:58:01 -0400

    solve 21.merge 2 sorted lists

---
+++ aug_2023/py_solutions.py
@@ -0,0 +1,27 @@
+# 21. Merge 2 sorted lists
+# https://leetcode.com/problems/merge-two-sorted-lists/description/
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# recursive solution:
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        if list1 is None:
+            return list2
+        if list2 is None:
+            return list1
+        if list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list2.next, list1)
+            return list2
