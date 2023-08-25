40d1aa823e573bf80e373313a35fe87b4f50209d
Simon Conrad <simondbconrad@gmail.com>
2023-08-25 15:17:19 -0400

    refactor sol_58 with save

--- aug_2023/py_solutions/sol_58.py
+++ aug_2023/py_solutions/sol_58.py
@@ -1,13 +1,10 @@
 # 58. Length of Last Word
 # https://leetcode.com/problems/length-of-last-word/description/
-# class Solution(object):
-def lengthOfLastWord(s):
-        # """
-        # :type s: str
-        # :rtype: int
-        # """
-    lst = s.split()
-    print(lst)
-    return len(lst[len(lst)-1])
-
-print(lengthOfLastWord("hello     world    sup?"))
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        lst = s.split()
+        return len(lst[len(lst)-1])
