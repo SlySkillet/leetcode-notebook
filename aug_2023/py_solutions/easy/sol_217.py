8520ec339e224065912980a4eaeb2815de6a8a9b
Simon Conrad <simondbconrad@gmail.com>
2023-08-30 22:30:42 -0400

    alternate solution

---
+++ aug_2023/py_solutions/easy/sol_217.py
@@ -0,0 +1,26 @@
+# 217. Contains Duplicate
+# https://leetcode.com/problems/contains-duplicate/description/
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        set_nums = set(nums)
+        if len(set_nums) == len(nums):
+            return False
+        return True
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        hashset = set()
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
