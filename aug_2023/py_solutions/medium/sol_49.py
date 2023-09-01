39dbf64adcead5a93ad9c6fc4e835f5daa73eafd
Simon Conrad <simondbconrad@gmail.com>
2023-09-01 11:44:08 -0400

    add additional solution to sol_49

--- aug_2023/py_solutions/medium/sol_49.py
+++ aug_2023/py_solutions/medium/sol_49.py
@@ -17,3 +17,21 @@ class Solution(object):
         for key in hashmap:
             result.append(hashmap[key])
         return result
+
+#  neetcode solution
+from collections import defaultdict
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        hashmap = defaultdict(list) #mapping charCount to list of Anagrams
+        for s in strs:
+            count = [0] * 26 #a ... z
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+
+            hashmap[tuple(count)].append(s)
+
+        return hashmap.values()
