ed3d728d7f8170539469e65484f88beb6c9eea64
Simon Conrad <simondbconrad@gmail.com>
2023-09-08 10:35:51 -0400

    add hashmap solution to 242

--- 2023/py_solutions/easy/sol_242.py
+++ 2023/py_solutions/easy/sol_242.py
@@ -11,3 +11,23 @@ class Solution(object):
         sorted_s=sorted([ord(n) for n in list(s)])
         sorted_t=sorted([ord(n) for n in list(t)])
         return sorted_s == sorted_t
+
+# hashmap solution
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        count_s, count_t = {}, {}
+        for i in range(len(s)):
+            count_s[s[i]] = 1 + count_s.get(s[i], 0)
+            count_t[t[i]] = 1 + count_t.get(t[i], 0)
+        for char in count_s:
+            print(char)
+            if count_s[char] != count_t.get(char):
+                return False
+        return True
