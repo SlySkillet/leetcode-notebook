7fabe2452ed02edd49f87f89f67b406587dbefd8
Simon Conrad <simondbconrad@gmail.com>
2023-09-11 10:12:18 -0400

    additional sol217

--- 2023/js_solutions/easy/sol_217.js
+++ 2023/js_solutions/easy/sol_217.js
@@ -11,3 +11,21 @@ var containsDuplicate = function(nums) {
     }
     return false
 };
+
+// improve runtime with a set
+var containsDuplicate = function(nums) {
+    let hashmap = new Set()
+    for (let num of nums){
+        if (hashmap.has(num)){
+            return true
+        }
+        hashmap.add(num)
+    }
+    return false
+};
+
+// solution comparing set to input array
+var containsDuplicate = function(nums) {
+    const s = new Set(nums);
+    return s.size !== nums.length;
+};
