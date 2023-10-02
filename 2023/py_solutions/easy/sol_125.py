09615e6ff6278aad5e2054e1ef1f80ee3d125985
Simon Conrad <simondbconrad@gmail.com>
2023-10-02 12:49:51 -0400

    new solution for 125

--- 2023/py_solutions/easy/sol_125.py
+++ 2023/py_solutions/easy/sol_125.py
@@ -17,3 +17,20 @@ class Solution:
             i += 1
             j -= 1
         return True
+
+def isPalindrome(self, s: str) -> bool:
+    def isalphanum(c):
+        return (ord('A') <= ord(c) <= ord('Z') or
+                ord('a') <= ord(c) <= ord('z') or
+                ord('0') <= ord(c) <= ord('9'))
+    i , j = 0 , len(s) - 1
+    while i < j:
+        while i < j and not isalphanum(s[i]):
+            i += 1
+        while j > i and not isalphanum(s[j]):
+            j -= 1
+        if s[i].lower() != s[j].lower():
+            return False
+        i, j = i + 1, j - 1
+
+    return True
