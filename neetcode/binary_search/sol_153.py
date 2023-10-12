a7110f1fe7d6c4cea2a3d2378f1bf3e3139e1f7d
Simon Conrad <simondbconrad@gmail.com>
2023-10-12 10:43:45 -0400

    add neetcode solution and annotate

--- neetcode/binary_search/sol_153.py
+++ neetcode/binary_search/sol_153.py
@@ -6,9 +6,27 @@ class Solution:
         l, r = 0, len(nums)- 1
         while l <= r:
             m = (l + r) // 2
-            if nums[m] < nums[m-1] or r == 0:
+            if nums[m] < nums[m-1] or r == 0: #finds the pivot point in the array
                 return nums[m]
             elif nums[m] < nums[r]:
                 r = m - 1
             else:
                 l = m + 1
+
+# neetcode solution with same run time:
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        res = nums[0]
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            if nums[l] < nums[r]:
+                res = min(res, nums[l])
+                break
+            m = (l + r) // 2
+            res = min(res, nums[m])
+            if nums[m] >= nums[l]:
+                l = m + 1
+            else:
+                r = m - 1
+        return res
