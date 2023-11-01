28520c300b05ea09203ecae6d7e115f6d050350e
Simon Conrad <simondbconrad@gmail.com>
2023-11-01 13:11:43 -0400

    recursive solution for 206

--- neetcode/linked_list/sol_206.py
+++ neetcode/linked_list/sol_206.py
@@ -15,3 +15,16 @@ class Solution:
             prev = curr
             curr = nxt
         return prev
+
+# recursive
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+
+        newHead = head
+        if head.next:
+            newHead = self.reverseList(head.next)
+            head.next.next = head
+        head.next = None
+        return newHead
