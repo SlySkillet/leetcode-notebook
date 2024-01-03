4de1ff0be7178f77574c4d9d8002325226b03b92
Simon Conrad <simondbconrad@gmail.com>
2024-01-03 14:03:53 -0500

    solve codewars kata

---
+++ code_wars/extract_domain_name.py
@@ -0,0 +1,23 @@
+# https://www.codewars.com/kata/514a024011ea4fb54200004b/train/python
+
+# 1st TAKE
+
+def domain_name(url):
+    lst = url.split("/")
+
+    str_core = ""
+    for s in lst:
+        if not (s == "http:" or s == "https:" or s == ""):
+            str_core += s
+            break
+
+    str_core = str_core.split(".")
+
+    if str_core[0] == "www":
+        return str_core[1]
+    else:
+        return str_core[0]
+
+"""
+First run through, this is done in linear time, iterations will never pass the first 3 items of the input.
+"""
