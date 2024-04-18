4410d0aaa9acbb5e149c7490707da7d77d14111b
Simon Conrad <simondbconrad@gmail.com>
2024-04-18 10:51:09 -0400

    rewrite sol_347 for practice

--- neetcode/second_pass/sol_347.py
+++ neetcode/second_pass/sol_347.py
@@ -14,5 +14,26 @@ class Solution():
                 if len(res) == k:
                     return res
 
-inst = Solution()
-print(inst.topKFrequent(nums = [1,1,1,2,2,3], k = 2))
+
+
+
+class Solution2():
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        hashMap = {} # value: count
+        for num in nums:
+            hashMap[num] = hashMap.get(num, 0) + 1
+
+        buckets = [[]for i in range(len(nums) + 1)]
+        for num, count in hashMap.items():
+            buckets[count].append(num)
+
+        res = []
+        for i in range(len(buckets) - 1, -1, -1):
+            for num in buckets[i]:
+                res.append(num)
+                if len(res) == k:
+                    print(res)
+                    return res
+
+inst = Solution2()
+inst.topKFrequent(nums=[5,5,5,4,4,3], k=2)
